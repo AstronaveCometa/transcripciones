2fbe6ec053ee69d68588be28fd6931e46d69289b
AstronaveCometa <sebastian.leon.castillo@gmail.com>
2024-06-18 17:19:43 -0400

    primer commit

---
+++ src/main/java/com/transcripciones/TranscripcionesApplication.java
@@ -0,0 +1,13 @@
+package com.transcripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscripcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscripcionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transcripciones/TranscripcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transcripciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscripcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
